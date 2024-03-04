24bfcd73c1fa24c01708e91754d22c6e73cea292
skarchoro <64647208+skarchoro@users.noreply.github.com>
2024-03-03 19:35:49 -0500

    Add files via upload

---
+++ encriptador-alura/script.js
@@ -0,0 +1,44 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const stringEncriptada=[]
+
+function btnEncriptar(){
+    
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o", "ober"],["u", "ufat"]];//console.table(matrizCodigo)
+    stringEncriptada=stringEncriptada.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+        
+    } 
+    return stringEncriptada
+}
+
+function btnDesencriptar(){
+    
+    const textoEncriptado = desencriptar(mensaje.value)
+    textArea.value = textoEncriptado
+    mensaje.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+function desencriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o", "ober"],["u", "ufat"]];//console.table(matrizCodigo)
+    stringEncriptada=stringEncriptada.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][1])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+        }
+        
+    } 
+    return stringEncriptada
+}
